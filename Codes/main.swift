266924d11f7c0a34674117a83ab8e352f8be85db
MixFon <mixfon@rambler.ru>
2022-05-17 09:30:30 +0300

    comments

--- Codes/main.swift
+++ Codes/main.swift
@@ -8,115 +8,115 @@
 import Foundation
 
 class Solution {
-    let chars = "([{"
-    let dict = [
-            ")":"(",
-            "]":"[",
-            "}":"{"
-        ]
-    func isValid(_ s: String) -> Bool {
-        var stack: [String] = []
-        for c in s {
-            if chars.contains(c) {
-                stack.append(String(c))
-            } else {
-                if stack.isEmpty { continue }
-                guard let last = stack.popLast() else { continue }
-                if last != dict[String(c)]! {
-                    return false
-                }
-            }
-        }
-        return stack.isEmpty
-    }
-    
-    func removeDuplicates(_ nums: inout [Int]) -> Int {
-        let set = Set<Int>(nums)
-        var arr: [Int]? = set.map({$0}).sorted()
-        for (i, c) in arr!.enumerated() {
-            let j = nums.index(nums.startIndex, offsetBy: i)
-            nums[j] = c
-        }
-        arr = nil
-        return set.count
-    }
-    
-    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
-        let arr = nums.filter( {$0 != val} )
-        for (i, c) in arr.enumerated() {
-            let j = nums.index(nums.startIndex, offsetBy: i)
-            nums[j] = c
-        }
-        return arr.count
-    }
-    
-    func maxSubArray(_ nums: [Int]) -> Int {
-        var s = 0
-        var resS = Int.min
-        for n in nums {
-            s += n
-            s = max(n, s)
-            resS = max(s, resS)
-        }
-        return resS
-    }
-    
-    func lengthOfLastWord(_ s: String) -> Int {
-        let arr = s.split{ $0 == " "}.map( { String($0) } )
-        guard let last = arr.last else { return 0 }
-        return last.count
-    }
-    
-    func plusOne(_ digits: [Int]) -> [Int] {
-        var div = 1
-        var result = [Int]()
-        for digit in digits.reversed() {
-            let num = (digit + div)
-            result.append(num % 10)
-            div = num / 10
-        }
-        if div != 0 { result.append(div) }
-        return result.reversed()
-    }
-    
-    // MARK: 67. Add Binary
-    private func stringToBinary(_ s: String) -> [Int] {
-            var num = [Int]()
-            for c in s {
-                if c == "1" {
-                    num.append(1)
-                } else {
-                    num.append(0)
-                }
-            }
-            return num
-        }
-        
-    func addBinary(_ a: String, _ b: String) -> String {
-        var one = stringToBinary(a)
-        var two = stringToBinary(b)
-        if one.count < two.count {
-            (one, two) = (two, one)
-        }
-        var div = 0
-        var result = [Int]()
-        for bitOne in one.reversed() {
-            let bitTwo: Int
-            if !two.isEmpty {
-                bitTwo = two.popLast()!
-            } else {
-                bitTwo = 0
-            }
-            let num = (bitOne + bitTwo + div)
-            result.append(num % 2)
-            div = num / 2
-        }
-        if div != 0 { result.append(div) }
-        var str = ""
-        result = result.reversed()
-        result.forEach( { str.append(String($0)) })
-        return str
-    }
+	let chars = "([{"
+	let dict = [
+		")":"(",
+		"]":"[",
+		"}":"{"
+	]
+	func isValid(_ s: String) -> Bool {
+		var stack: [String] = []
+		for c in s {
+			if chars.contains(c) {
+				stack.append(String(c))
+			} else {
+				if stack.isEmpty { continue }
+				guard let last = stack.popLast() else { continue }
+				if last != dict[String(c)]! {
+					return false
+				}
+			}
+		}
+		return stack.isEmpty
+	}
+	
+	func removeDuplicates(_ nums: inout [Int]) -> Int {
+		let set = Set<Int>(nums)
+		var arr: [Int]? = set.map({$0}).sorted()
+		for (i, c) in arr!.enumerated() {
+			let j = nums.index(nums.startIndex, offsetBy: i)
+			nums[j] = c
+		}
+		arr = nil
+		return set.count
+	}
+	
+	func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+		let arr = nums.filter( {$0 != val} )
+		for (i, c) in arr.enumerated() {
+			let j = nums.index(nums.startIndex, offsetBy: i)
+			nums[j] = c
+		}
+		return arr.count
+	}
+	
+	func maxSubArray(_ nums: [Int]) -> Int {
+		var s = 0
+		var resS = Int.min
+		for n in nums {
+			s += n
+			s = max(n, s)
+			resS = max(s, resS)
+		}
+		return resS
+	}
+	
+	func lengthOfLastWord(_ s: String) -> Int {
+		let arr = s.split{ $0 == " "}.map( { String($0) } )
+		guard let last = arr.last else { return 0 }
+		return last.count
+	}
+	
+	func plusOne(_ digits: [Int]) -> [Int] {
+		var div = 1
+		var result = [Int]()
+		for digit in digits.reversed() {
+			let num = (digit + div)
+			result.append(num % 10)
+			div = num / 10
+		}
+		if div != 0 { result.append(div) }
+		return result.reversed()
+	}
+	
+	// MARK: 67. Add Binary
+	private func stringToBinary(_ s: String) -> [Int] {
+		var num = [Int]()
+		for c in s {
+			if c == "1" {
+				num.append(1)
+			} else {
+				num.append(0)
+			}
+		}
+		return num
+	}
+	
+	func addBinary(_ a: String, _ b: String) -> String {
+		var one = stringToBinary(a)
+		var two = stringToBinary(b)
+		if one.count < two.count {
+			(one, two) = (two, one)
+		}
+		var div = 0
+		var result = [Int]()
+		for bitOne in one.reversed() {
+			let bitTwo: Int
+			if !two.isEmpty {
+				bitTwo = two.popLast()!
+			} else {
+				bitTwo = 0
+			}
+			let num = (bitOne + bitTwo + div)
+			result.append(num % 2)
+			div = num / 2
+		}
+		if div != 0 { result.append(div) }
+		var str = ""
+		result = result.reversed()
+		result.forEach( { str.append(String($0)) })
+		return str
+	}
 	
 	// MARK: 69. Sqrt(x)
 	func mySqrt(_ x: Int) -> Int {
@@ -131,40 +131,114 @@ class Solution {
 	}
 	
 	
-	 public class ListNode {
-	     public var val: Int
-	     public var next: ListNode?
-	     public init() { self.val = 0; self.next = nil; }
-	     public init(_ val: Int) { self.val = val; self.next = nil; }
-	     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
-	 }
+	public class ListNode {
+		public var val: Int
+		public var next: ListNode?
+		public init() { self.val = 0; self.next = nil; }
+		public init(_ val: Int) { self.val = val; self.next = nil; }
+		public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+	}
 	
 	func deleteDuplicates(_ head: ListNode?) -> ListNode? {
-			var elem = head
-			var uniqueElemts = Set<Int>()
-			while elem != nil {
-				print(elem!.val)
-				print(uniqueElemts)
-				//uniqueElemts.insert()
-				uniqueElemts = uniqueElemts.intersection(Set<Int>([head!.val]))
-				elem = elem!.next
-			}
-			var arr = [Int]()
-			uniqueElemts.forEach({arr.append($0)})
-			print(arr)
-			var newHead: ListNode? = nil
-			for num in arr {
-				if newHead == nil {
-					newHead = ListNode(num)
-				} else {
-					let node = ListNode(num)
-					node.next = newHead
-					newHead = node
-				}
-			}
-			return newHead
+		var elem = head
+		var uniqueElemts = Set<Int>()
+		while elem != nil {
+			print(elem!.val)
+			print(uniqueElemts)
+			//uniqueElemts.insert()
+			//uniqueElemts = uniqueElemts.intersection(Set<Int>([head!.val]))
+			elem = elem!.next
 		}
+		var arr = [Int]()
+		uniqueElemts.forEach({arr.append($0)})
+		print(arr)
+		var newHead: ListNode? = nil
+		for num in arr {
+			if newHead == nil {
+				newHead = ListNode(num)
+			} else {
+				let node = ListNode(num)
+				node.next = newHead
+				newHead = node
+			}
+		}
+		return newHead
+	}
+	func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+		var i = 0
+		var j = 0
+		var k = 0
+		var newArr = [Int]()
+		while i < m || j < n {
+			if i < m && j < n {
+				if nums1[i] < nums2[j] {
+					newArr.append(nums1[i])
+					i += 1
+				} else if nums1[i] == nums2[j] {
+					newArr.append(nums1[i])
+					newArr.append(nums2[j])
+					i += 1
+					j += 1
+				} else {
+					newArr.append(nums2[j])
+					j += 1
+				}
+			} else if i < m && !(j < n) {
+				newArr.append(nums1[i])
+				i += 1
+			} else if !(i < m) && j < n {
+				newArr.append(nums2[j])
+				j += 1
+			}
+		}
+		while k < (m + n) {
+			nums1[k] = newArr[k]
+			k += 1
+		}
+	}
 	
+	private func getNum(_ num: Int) -> Int {
+		var a = num
+		var b = 0
+		while a != 0 {
+			b += (a % 10) * (a % 10)
+			a = a / 10
+		}
+		return b
+	}
+	
+	func isHappy(_ n: Int) -> Bool {
+		var c = n
+		var set = Set<Int>()
+		while c != 1 && !set.contains(c) {
+			set.insert(c)
+			c = getNum(c)
+		}
+		return c == 1
+	}
+	func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+		if head == nil { return nil }
+		var result = head
+		var iter = head
+		while iter != nil {
+			if iter!.val == val {
+				result = result!.next
+			} else {
+				break
+			}
+			iter = iter!.next
+		}
+		if iter == nil { return nil }
+		iter = result
+		while iter!.next != nil {
+			if iter!.next!.val == val {
+				iter!.next = iter!.next!.next
+				continue
+			}
+			iter = iter!.next
+		}
+		return result
+	}
 }
 let one = Solution()
 var arr = [1,1,2]
